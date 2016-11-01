f894f026ccd3ffab0e4d2425f183304a9a4cba7a
Branden Pinney <bapinney@gmail.com>
2016-11-01 10:03:24 -0700

    Need to change from sessionStorage to localStorage and use JSON.stringify
    Taking a snapshot before changing any code.

--- script.jsx
+++ script.jsx
@@ -1,13 +1,67 @@
 class RecipeBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    componentWillMount() {
+        console.log("Inside RecipeBox cWM...");
+        console.log("Checking if recipies exist in local storage...");
+        var recipies;
+        if (sessionStorage.getItem("recipies") == null) {
+            console.log("No recipies in local storage... creating initial recipies...");
+            recipies = 
+                [{name: "Club Sandwich",
+                  ingredients: ["bread", "lettuce", "turkey", "ham", "bacon"]},
+                 {name: "Chicken Soft Tacos",
+                  ingredients: ["chicken", "lettuce", "cheddar cheese", "secret sauce", "tortilla"]},
+                 {name: "Powerpuff Girls",
+                  ingredients: ["sugar", "spice", "everything nice", "chemical x"]}
+                 ];
+            console.log("before");
+            console.dir(recipies);
+            sessionStorage.setItem("recipies", recipies);
+            console.log("after");
+            var foo = sessionStorage.getItem("recipies");
+            console.dir(foo);
+        }
+        else {
+            console.log("Initializing recipies with those in local storage...");
+            recipies = sessionStorage.getItem("recipies");
+        }
+        var rlOutput = [];
+        console.log("Here is recipies");
+        console.dir(recipies);
+        for (var i=0; i<recipies.length; i++) {
+            console.log(`At i ${i}`);
+            rlOutput.push((<Recipe key={i.toString()} name={recipies[i]["name"]} tabIndex="0">{recipies[i]["name"]}</Recipe>));
+        }
+        this.setState({"recipiesList": rlOutput});
+    }
+    
     render() {
-        var recipies = ['chicken', 'liver', 'meow mix'];
+        console.dir("At render");
+        console.dir(this.state);
         return (
             <div className="recipebox">
-                {recipies.map(function(name, index) {
-                    return (<div key={index}>{name}</div>);
-                })}
+                {this.state.recipiesList}
             </div>
         )
+    }
+}
+
+class Recipe extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("diring props of recipe");
+        console.dir(props);
+    }
+    
+    componentWillMount() {
+        console.log("Inside Recipe cWM...");
+    }
+    
+    render() {
+        return (<div tabIndex="0">{"Recipie for " + this.props.name}</div>)
     }
 }
 
