b7573824cfe494d03bb193ee1156918598d931ab
Branden Pinney <bapinney@gmail.com>
2016-11-02 16:50:56 -0700

    Switched from sessionStorage to localStorage
    You don’t want the list to go away right after you leave it.

--- script.jsx
+++ script.jsx
@@ -1,13 +1,14 @@
 class RecipeBox extends React.Component {
     constructor(props) {
         super(props);
+        console.log("inside const");
     }
     
     componentWillMount() {
         console.log("Inside RecipeBox cWM...");
         console.log("Checking if recipies exist in local storage...");
         var recipies;
-        if (sessionStorage.getItem("recipies") == null) {
+        if (localStorage.getItem("recipies") == null) {
             console.log("No recipies in local storage... creating initial recipies...");
             recipies = 
                 [{name: "Club Sandwich",
@@ -19,21 +20,21 @@ class RecipeBox extends React.Component {
                  ];
             console.log("before");
             console.dir(recipies);
-            sessionStorage.setItem("recipies", recipies);
+            localStorage.setItem("recipies", JSON.stringify(recipies));
             console.log("after");
-            var foo = sessionStorage.getItem("recipies");
+            var foo = JSON.parse(localStorage.getItem("recipies"));
             console.dir(foo);
         }
         else {
             console.log("Initializing recipies with those in local storage...");
-            recipies = sessionStorage.getItem("recipies");
+            recipies = JSON.parse(localStorage.getItem("recipies"));
         }
         var rlOutput = [];
         console.log("Here is recipies");
         console.dir(recipies);
         for (var i=0; i<recipies.length; i++) {
             console.log(`At i ${i}`);
-            rlOutput.push((<Recipe key={i.toString()} name={recipies[i]["name"]} tabIndex="0">{recipies[i]["name"]}</Recipe>));
+            rlOutput.push((<Recipe key={i.toString()} recipie={recipies[i]} tabIndex="0">{recipies[i]["name"]}</Recipe>));
         }
         this.setState({"recipiesList": rlOutput});
     }
@@ -54,14 +55,56 @@ class Recipe extends React.Component {
         super(props);
         console.log("diring props of recipe");
         console.dir(props);
+        this.showIngredients = this.showIngredients.bind(this);
     }
     
     componentWillMount() {
         console.log("Inside Recipe cWM...");
+        this.setState({ingredientsList: null});
+        console.dir(this);
     }
     
     render() {
-        return (<div tabIndex="0">{"Recipie for " + this.props.name}</div>)
+        return (<div onClick={this.toggleIngredients.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} tabIndex="0">{"Recipie for " + this.props.recipie.name}
+        {this.state.ingredientsList}
+        </div>)
+    }
+    
+    handleKeyPress(e) {
+        if (e.charCode == 13) {
+            this.toggleIngredients(e);
+        }
+    }
+    
+    toggleIngredients(e) {
+        if (this.state.ingredientsList == null) {
+            this.showIngredients(e);
+        }
+        else {
+            this.hideIngredients(e);
+        }
+    }
+    
+    hideIngredients(e) {
+        console.log("hide!");
+        this.setState({ingredientsList: null});
+    }
+    
+    showIngredients(e) {
+        console.log("show Ings called");
+        console.dir(e.target);
+        console.dir(this);    
+        console.dir(this.refs);
+        var rNode = ReactDOM.findDOMNode(this);
+        console.dir(rNode);
+        var ingredients = <IngredientsList ingredients={this.props.recipie.ingredients}></IngredientsList>;
+        this.setState({ingredientsList: ingredients});
+    }
+}
+
+class IngredientsList extends React.Component {
+    render() {
+        return (<div>ingredients list!!!</div>)
     }
 }
 
